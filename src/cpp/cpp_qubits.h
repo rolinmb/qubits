ad54bfffdb6a46c032ba23fa7261ce545d4d81e1
Rolin Blake <rolinmblake@gmail.com>
2023-08-16 20:02:45 -0500

    updated c++ to validate QuantumRegister

--- src/cpp/cpp_qubits.h
+++ src/cpp/cpp_qubits.h
@@ -1,6 +1,19 @@
 #include <complex>
 #include <string>
+#include <vector>
 #include <Windows.h>
+
+float genRandom() {
+    LARGE_INTEGER performanceCount;
+    FILETIME systemTime;
+    ULONGLONG combinedTime;
+    QueryPerformanceCounter(&performanceCount);
+    GetSystemTimeAsFileTime(&systemTime);
+    combinedTime = ((ULONGLONG)systemTime.dwHighDateTime << 32) + systemTime.dwLowDateTime + performanceCount.QuadPart;
+    srand((unsigned int)combinedTime);
+    Sleep(1);
+    return (float)rand() / RAND_MAX;
+}   
 
 class Qubit
 {
@@ -20,18 +33,6 @@ public:
         }
     }
 
-    float genRandom() {
-        LARGE_INTEGER performanceCount;
-        FILETIME systemTime;
-        ULONGLONG combinedTime;
-        QueryPerformanceCounter(&performanceCount);
-        GetSystemTimeAsFileTime(&systemTime);
-        combinedTime = ((ULONGLONG)systemTime.dwHighDateTime << 32) + systemTime.dwLowDateTime + performanceCount.QuadPart;
-        srand((unsigned int)combinedTime);
-        Sleep(1);
-        return (float)rand() / RAND_MAX;
-    }   
-
     int measure() {
         if (this->C0.real() == 1 && this->C0.imag() == 0 && this->C1.real() == 0 && this->C1.imag() == 0)
         {
@@ -41,7 +42,7 @@ public:
         {
             return 1;
         }
-        float observation = genRandom(); // TODO: re-implement random number generator in c++
+        float observation = genRandom();
         float prob0 = abs(this->C0)*abs(this->C0);
         float prob1 = abs(this->C1)*abs(this->C1);
         if (prob0 == prob1)
@@ -65,5 +66,50 @@ public:
     void toString()
     {
         printf("\n[{%f + %fi}, {%f + %fi}]", this->C0.real(), this->C0.imag(), this->C1.real(), this->C1.imag());
+    }
+};
+
+Qubit singletonQubit(std::complex<float> c0, std::complex<float> c1) {
+    if (round(abs(c0)*abs(c0) + abs(c1)*abs(c1)) != 1)
+        {
+            throw std::invalid_argument("Invalid complex number parameters to construct Qubit.");
+        }
+    return Qubit(c0, c1);
+}
+
+class QuantumRegister
+{
+public:
+    std::vector<Qubit> qubits;
+
+    QuantumRegister(std::vector<Qubit> qbits)
+    {
+        if (validateRegister(qbits))
+        {
+            this->qubits = qbits;
+        }
+        else
+        {
+            throw std::invalid_argument("Invalid Qubits vector, cannot form QuantumRegister");
+        } 
+    }
+
+    void toString()
+    {
+        printf("\n~\n");
+        for (size_t i = 0; i < this->qubits.size(); i++) {
+            printf("[{%f + %fi}, {%f + %fi}]\n", this->qubits[i].C0.real(), this->qubits[i].C0.imag(), this->qubits[i].C1.real(), this->qubits[i].C1.imag());
+        }
+        printf("~\n");
+    }
+
+private:
+    bool validateRegister(std::vector<Qubit> qbits)
+    {
+        float sum = 0;
+        for (size_t i = 0; i < qbits.size(); i++) {
+            sum += abs(qbits[i].C0)*abs(qbits[i].C0) + abs(qbits[i].C1)*abs(qbits[i].C1);
+        }
+        return (round(sum) == 1) ? true : false;
     }
 };
--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,9 +1,15 @@
 #include "cpp_qubits.h"
-#include <iostream>
 
 int main()
 {
-    Qubit q0(std::complex<float>(1.f, 0.f), std::complex<float>(0.f, 0.f));
+    Qubit q0 = singletonQubit(std::complex<float>(1.f, 0.f), std::complex<float>(0.f, 0.f));
     q0.toString();
+    Qubit q1 = Qubit(std::complex<float>(-1.f/2.f, 0.f), std::complex<float>(0.f, 1.f/2.f));
+    Qubit q2 = Qubit(std::complex<float>(0.f, 1.f/2.f), std::complex<float>(-1.f/2.f, 0.f));
+    std::vector<Qubit> qbits;
+    qbits.push_back(q1);
+    qbits.push_back(q2);
+    QuantumRegister qr = QuantumRegister(qbits);
+    qr.toString();
     return 0;
 }
