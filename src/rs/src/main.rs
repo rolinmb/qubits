9643bd6beec73fc654275e301e5f584fc6f6b6a4
Rolin Blake <rolinmblake@gmail.com>
2023-08-16 20:50:52 -0500

    implemented QuantumRegister in rust

--- src/rs/src/main.rs
+++ src/rs/src/main.rs
@@ -2,37 +2,66 @@ use num_complex::Complex64;
 
 #[derive(Debug)]
 struct Qubit {
-    C0: Complex64,
-    C1: Complex64
+    c0: Complex64,
+    c1: Complex64
 }
 
-fn validate_qubit(c0: Complex64, c1: Complex64) -> bool {
-    if f64::round(c0.norm()*c0.norm() + c1.norm()*c1.norm()) as i32 == 1 {
+fn validate_qubit(cmp0: Complex64, cmp1: Complex64) -> bool {
+    if f64::round(cmp0.norm()*cmp0.norm() + cmp1.norm()*cmp1.norm()) as i32 == 1 {
         return true;
     } else {
         return false;
     }
 }
 
-fn new_qubit(c0: Complex64, c1: Complex64) -> Qubit {
-    let mut qbit = Qubit{C0: c0, C1: c1};
-    if !validate_qubit(c0, c1) {
-        qbit.C0 = Complex64::new(0.0, 0.0);
-        qbit.C1 = Complex64::new(0.0, 0.0);
+fn singleton_qubit(cmp0: Complex64, cmp1: Complex64) -> Qubit {
+    let mut qbit = Qubit{c0: cmp0, c1: cmp1};
+    if !validate_qubit(cmp0, cmp1) {
+        qbit.c0 = Complex64::new(0.0, 0.0);
+        qbit.c1 = Complex64::new(0.0, 0.0);
     }
     return qbit;
 }
 
+#[derive(Debug)]
+struct QuantumRegister {
+    qubits: Vec<Qubit>
+}
+
+fn validate_register(qbits: &Vec<Qubit>) -> bool {
+    let mut sum = 0.0;
+    for q in qbits {
+        sum += q.c0.norm()*q.c0.norm() + q.c1.norm()*q.c1.norm();
+    }
+    (f64::round(sum)) as i32 == 1
+}
+
+fn new_qregister(qbits: Vec<Qubit>) -> QuantumRegister {
+    if validate_register(&qbits) {
+        return QuantumRegister{qubits: qbits};
+    } else {
+        return QuantumRegister{qubits: Vec::new()};
+    }
+}
+
 fn main() {
-    let c0 = Complex64::new(1.0, 0.0);
-    let c1 = Complex64::new(0.0, 1.0);
-    let mut q = Qubit{C0: c0, C1: c1};
-    println!("First {:?}", q);
-    q = new_qubit(c1, c0);
-    println!("Second {:?}", q);
-    let c2 = Complex64::new(0.0, 0.0);
-    q = new_qubit(c0, c2);
-    println!("Third {:?}", q);
-    q = new_qubit(c2, c0);
-    println!("Fourth {:?}", q);
+    let cmp0 = Complex64::new(1.0, 0.0);
+    let cmp1 = Complex64::new(0.0, 1.0);
+    let mut q0 = Qubit{c0: cmp0, c1: cmp1};
+    println!("First {:?}", q0);
+    q0 = singleton_qubit(cmp1, cmp0);
+    println!("Second {:?}", q0);
+    let cmp2 = Complex64::new(0.0, 0.0);
+    q0 = singleton_qubit(cmp0, cmp2);
+    println!("Third {:?}", q0);
+    q0 = singleton_qubit(cmp2, cmp0);
+    println!("Fourth {:?}", q0);
+    let cmp3 = Complex64::new(0.5, 0.0);
+    let cmp4 = Complex64::new(0.0, -0.5);
+    let qbits: Vec<Qubit> = vec![
+        Qubit {c0: cmp3, c1: cmp4},
+        Qubit {c0: cmp4, c1: cmp3}
+    ];
+    let qr = new_qregister(qbits);
+    println!("{:?}", qr)
 }
