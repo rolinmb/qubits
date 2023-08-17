208313b1b26bbd294c91a22d94ca6c695bfde8e1
Rolin Blake <rolinmblake@gmail.com>
2023-08-16 20:21:50 -0500

    updated main.go to correct QuantumRegister implementation

--- src/go/main.go
+++ src/go/main.go
@@ -13,7 +13,7 @@ type Qubit struct {
     C1 complex128
 }
 
-func newQubit(c0, c1 complex128) (*Qubit, error) {
+func singletonQubit(c0, c1 complex128) (*Qubit, error) {
     if math.Round(cmplx.Abs(c0)*cmplx.Abs(c0) +  cmplx.Abs(c1)*cmplx.Abs(c1)) == 1 {
         q := &Qubit{C0: c0, C1: c1}
         return q, nil
@@ -52,17 +52,46 @@ func qubitToString(q *Qubit) {
     fmt.Printf("\n[{%f + %fi}, {%f + %fi]\n", real(q.C0), imag(q.C0), real(q.C1), imag(q.C1))
 }
 
+type QuantumRegister struct {
+    qubits []Qubit
+}
+
+func validateRegister(qbits []Qubit) bool {
+    sum := 0.0
+    for i := 0; i < len(qbits); i++ {
+        sum += cmplx.Abs(qbits[i].C0)*cmplx.Abs(qbits[i].C0) + cmplx.Abs(qbits[i].C1)*cmplx.Abs(qbits[i].C1)
+    }
+    if math.Round(sum) == 1 {
+        return true
+    } else {
+        return false
+    }
+}
+
+func registerToString(qr QuantumRegister) {
+    fmt.Println("\n~")
+    for i:= 0; i < len(qr.qubits); i++ {
+        fmt.Printf("\n[{%f + %fi}, {%f + %fi]\n", real(qr.qubits[i].C0), imag(qr.qubits[i].C0), real(qr.qubits[i].C1), imag(qr.qubits[i].C1))
+    }
+    fmt.Println("~\n")
+}
+
 func main() {
     c0 := complex(0.0, 0.0)
     c1 := complex(0.0, 1.0)
-    q, err := newQubit(c0, c1)
+    q0, err := singletonQubit(c0, c1)
     if err != nil {
         fmt.Println("Error: ", err)
         return
     }
-    observation := getObservation()
-    fmt.Println(observation)
-    measurement := measure(q)
+    measurement := measure(q0)
     fmt.Println(measurement)
-    qubitToString(q)
+    qubitToString(q0)
+    c2 := complex(-0.5, 0.0)
+    c3 := complex(0.0, 0.5)
+    q1 := Qubit{C0: c2, C1: c3}
+    q2 := Qubit{C0: c3, C1: c2}
+    qbits := []Qubit{q1, q2}
+    qr := QuantumRegister{qubits: qbits}
+    registerToString(qr)
 }
