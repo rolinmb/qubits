ebad034bfa1103220727a989d317b9cda0cd0e17
Rolin Blake <rolinmblake@gmail.com>
2024-04-24 19:55:34 -0500

    messing around with qregister

--- src/py/qregister.py
+++ src/py/qregister.py
@@ -55,17 +55,23 @@ class qRegister:
             outcome = qubit.measure()
             collapsed_state += str(outcome)
         return collapsed_state
-
+    """
     def __repr__(self):
         representation = ""
         for qubit in self.qubits:
             representation += f"{qubit}\n"
         return representation
-    
+    """
+    def __repr__(self):
+        representation = "\n"
+        for i in range(0, self.n_qubits):
+            representation += f"([{round(self.qubits[i].c0.real, 5)} + {round(self.qubits[i].c0.imag, 5)}i]; [{round(self.qubits[i].c1.real, 5)} + {round(self.qubits[i].c1.imag, 5)}i])\n"
+        return representation
+        
 if __name__ == "__main__":
-    register = qRegister([ # 25% chance to collapse to 00, 01, 10, 11
-        qubit(0.5+0j, 0-0.5j),
-        qubit(-0.5+0j, 0+0.5j)])
+    qubits = [qubit(0.5+0j, 0-0.5j), qubit(-0.5+0j, 0+0.5j)]
+    register = qRegister(qubits) # 25% chance to collapse to 00, 01, 10, 11
     print(register)
     for i in range(0, 100):
-        print(f"Quantum Register Measurement {i}: {register.measure()}")
+        collapsed_state = register.measure()
+        print(f"Quantum Register Measurement {i}: {collapsed_state}")
